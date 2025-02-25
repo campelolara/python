60138cb952c1196c4e878ca7db3cf0754be24c8e
Lara <161225698+campelolara@users.noreply.github.com>
2025-02-25 11:07:58 -0300

    exercicios

---
+++ codigos/main.py
@@ -0,0 +1,62 @@
+"""
+#ex002 e ex003 = soma, subtração, multiplicação e divisão
+num1 = float(input("Informe um número: "))
+num2 = float(input("Informe outro número: "))
+
+########################################################
+
+soma = num1 + num2 
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+media = (num1 + num2) / 2
+
+print(f"A soma dos dois números é {soma}")
+print(f"A subtração dos dois números é {subtracao}")
+print(f"A multiplicação dos dois números é {multiplicacao}")
+print(f"A divisão dos dois números é {divisao}")
+print(f"A média dos dois números é {media}")
+
+########################################################
+
+#ex001 e ex004 =  nome, idade e cidade
+nome = input("Qual o seu nome: ")
+idade = int(input("Qual a sua idade: "))
+cidade = input("Em qual cidade você mora: ")
+
+print(f" Olá, seja bem-vindo(a) {nome}. Você tem {idade} anos e mora em {cidade}")
+
+########################################################
+
+#ex006 = calcular a área de um retângulo (base × altura)
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+area = base * altura
+
+print(f"A área do rentângulo é {area}")
+
+########################################################
+
+#ex007 = Celsius para Fahrenheit - F = (C × 9/5) + 32
+celsius = float(input("Digite a temperatura em grau Celsius: "))
+
+convert_fahrenheit = (celsius * 9/5) + 32
+
+print(f"O grau em Celsius convertido para Fahrenheit é {convert_fahrenheit}")
+
+########################################################
+
+#ex008 = Cálculo do Perímetro de um Quadrado - 4 × lado
+lado = float(input("Quanto é o lado do quadrado: "))
+
+perimetro = 4 * lado
+
+print(f"O perímetro do quadrado é: {perimetro}")
+
+"""
+
+#ex009 = Conversão Real para Dolar
+
+
+
