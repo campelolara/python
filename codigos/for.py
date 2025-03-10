5ca7920fc05610f7759080da7e77af0b3d2a77bb
Lara <161225698+campelolara@users.noreply.github.com>
2025-03-09 23:25:46 -0300

    aaa

--- codigos/for.py
+++ codigos/for.py
@@ -18,25 +18,133 @@ for produto in listaSupermercado:
     print(f"Produto: {produto}")
 
 """
+
+
 ### FUNÇÕES 
-#ex001 = intervalo de uma lista com números pares
+#ex001 = Números pares e ímpares de um intervalo
 def intervalo(valorInicial, valorFinal):
-    lista_intervalo = []
-    for i in range(valorInicial, valorFinal):
+    lista_par = []
+    lista_impar = []
+    for i in range(valorInicial, valorFinal+1):
         if i % 2 == 0:
-            lista_intervalo.append(i)
-    print(lista_intervalo)
+            lista_par.append(i)
+        else:
+            lista_impar.append(i)
+    print(lista_par)
+    print(lista_impar)
 
+#ex002 = Tabuada de Multiplicação
+def tabuadaMultiplicacao(num):
+    for i in range(11):
+        multiplicacao = num * i
+        print(f"{num} x {i} = {multiplicacao}")
 
+#ex003 = Números ímpares entre 100 e 200
+def numerosImpares():
+    numImpar = []
+    for i in range(100, 200):
+        if i % 2 != 0:
+            numImpar.append(i)
+    print(numImpar)
+
+#ex004 = Contador
+def contador(num):
+    cont = []
+    for i in range(num+1):
+        cont.append(i)
+    print(cont)
+    
+#ex005 = Soma dos números
+def soma():
+    numeros = []
+    for i in range(5):
+        numeros.append(int(input("Digite um número: ")))
+    print(f"Os números digitados foram: {numeros}")
+    print(f"A soma dos números é: {sum(numeros)}")
+
+#ex006 = fatorial
+def fatorial(num):
+    #fatoria inicia com 1
+    fat = 1
+    #para cada numero de 1 até num
+    for i in range(1, num+1):  
+        fat *= i
+    print(fat)
+
+#ex007 = Numeros pares e ímpares de 1 até num
+def parImpar(num):
+    lista_par = []
+    lista_impar = []
+    for i in range(1, num+1):
+        if i % 2 == 0:
+            lista_par.append(i)
+        else:
+            lista_impar.append(i)
+    print(f"Os números pares são: {lista_par}")
+    print(f"Os números ímpares são: {lista_impar}")
+
+#ex008 = Divisores de um Número
+def divisores(num):
+    lista_divisores = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            lista_divisores.append(i)
+    print(f"O divisores de {num} são: {lista_divisores}")
+                
+
+        
 ### CORPO PRINCIPAL
 print("---------OPÇÕES---------")
-print("1. Valores pares")
+print("1. Par e Impar dentro de um intervalor")
+print("2. Tabuada de multiplicação")
+print("3. Números ímpares entre 100 e 200")
+print("4. Contador")
+print("5. Soma dos números")
+print("6. Fatorial")
+print("7. Par e Impar de 1 até um número")
+print("8. Divisores de um número")
 
 escolha = int(input("Digite um numero: "))
 
 if escolha == 1:
-    print("----Valores pares----")
-    valorI = int(input("Digite o valor inicial: "))
-    valorF = int(input("Digite o valor final: "))
-    intervalo(valorI, valorF)
+    print("----Par e Impar num intervalo----")
+    numI = int(input("Digite o valor inicial: "))
+    numF = int(input("Digite o valor final: "))
+    intervalo(numI, numF)
+
+elif escolha == 2:
+    print("----Tabuada de multiplicação----")
+    num = int(input("Digite um valor: "))
+    tabuadaMultiplicacao(num)
+
+elif escolha == 3:
+    print("----Números ímpares entre 100 e 200----")
+    numerosImpares()
+
+elif escolha == 4:
+    print("----Contador----")
+    num = int(input("Digite um valor: "))
+    contador(num)
+
+elif escolha == 5:
+    print("----Soma dos números----")
+    soma()
+
+elif escolha == 6:
+    print("----Fatorial----")
+    num = int(input("Digite um número:"))
+    fatorial(num)
+
+elif escolha == 7:
+    print("----Par e Impar de 1 até um número----")
+    num = int(input("Digite um número:"))
+    parImpar(num)
+
+elif escolha == 8:
+    print("----Divisores de um número----")
+    num = int(input("Digite um número:"))
+    divisores(num)
+
+
+
 
--- codigos/while.py
+++ codigos/while.py
@@ -1,53 +1,89 @@
-#### EXERCÍCIOS COM ESTRUTURA DE REPETIÇÃO WHILE ####
-
 ###FUNÇÕES
-#Qual número é maior e qual é o menor
-def maiorMenor():
+#ex001 = Contador e Soma
+def contadorSoma():
+    num = int(input("Digite um número:"))
     lista = []
     contador = 1
-
-    while contador <= 3:
-        num = int(input("Digite um numero: "))
-        lista.append(num)
+    while contador <= num:
+        lista.append(contador)
         contador += 1
+    print(f"A contagem dos números é: {lista}")
+    print(f"A soma dos número é: {sum(lista)}")
 
-    maior_elemento = max(lista)
-    menor_elemento = min(lista)
-    print(f"O maior elemento é: {maior_elemento}")
-    print(f"O menor elemento é: {menor_elemento}")
+#ex002 = Leitura de números positivos
+def leituraNumeros():
+    num = int(input("Digite um número (valor negativo encerra o código):"))
+    lista_numeros = []
+
+    while num >= 0:
+        lista_numeros.append(num)
+        num = int(input("Digite um número (valor negativo encerra o código):"))
+
+    print(f"Os números digitados foram: {lista_numeros}")
+    print(f"A quantidade de números positivos digitados foi: {len(lista_numeros)}")
+
+#ex003 = Soma de números (0 para)
+def somaNumeros():
+    numeros = []
+    num = int(input("Digite um número (0 encerra): "))
+    while num != 0:
+        numeros.append(num)
+        num = int(input("Digite um número (0 encerra): "))
     
-#Ordenação de Listas
-def lista():
-    lista = []
-    contador = 1
+    print(f"Os números digitados foram: {numeros}")
+    print(f"A soma dos números é: {sum(numeros)}")
+        
 
-    while contador <= 5:
-        lista.append(int(input("Digite valores: ")))
-        contador += 1
+#ex004 = Validação de senha
+def validacaoSenha():
+    senha = input("Digite uma senha:")
+    while senha != "1234":
+        print("Senha inválida")
+        senha = input("Digite uma senha:")
+    if senha == "1234":
+        print("Senha correta")
 
-    print(lista)
+#ex005 = Contagem regressiva
+def contagemRegressiva():
+   num = int(input("Digite um número:"))
+   lista = []
+   
+   while num >= 0:
+    lista.append(num)
+    num -= 1
+    
+    print(f"A contagem regressiva dos números é: {lista}")
 
-    lista.sort()
-    print("Lista Ordenada", lista)
-
-    lista.sort(reverse=True)
-    print("Lista Ordenada Inversamente", lista)
-
-    print("Maior elemento da lista", max(lista))
 
 
-
-### CORPO PRINCIPAL
+###CORPO PRINCIPAL
 print("---------OPÇÕES---------")
-print("1. Maior e menor número")
-print("2. Listas")
+print("1. Contador e Soma")
+print("2. Quantidade de números positivos")
+print("3. Soma de números")
+print("4. Validação de Senha")
+print("5. Contagem Regressiva")
 
 escolha = int(input("Digite um numero: "))
 
 if escolha == 1:
-    print("----Maior e menor número----")
-    maiorMenor()
+    print("----Contador e Soma----")
+    contadorSoma()
+
 elif escolha == 2:
-    print("-----------Listas-----------")
-    lista()
-    
+    print("----Quantidade de números positivos----")
+    leituraNumeros()
+
+elif escolha == 3:
+    print("----Soma de números----")
+    somaNumeros()
+
+elif escolha == 4:
+    print("----Validação de Senha----")
+    validacaoSenha()
+
+elif escolha == 5:
+    print("----Contagem Regressiva----")
+    contagemRegressiva()
+
+
